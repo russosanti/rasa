1ff83043ee66d36d374e32d9ef28f3d562748947
Tanja Bergmann <tabergma@gmail.com>
2020-03-09 15:08:42 +0100

    update predict entities

--- rasa/nlu/classifiers/diet_classifier.py
+++ rasa/nlu/classifiers/diet_classifier.py
@@ -778,6 +778,7 @@ class DIETClassifier(IntentClassifier, EntityExtractor):
                         "start": token.start,
                         "end": token.end,
                         "extractor": "DIET",
+                        "sub_entity": NO_ENTITY_TAG,
                     }
                 entities.append(entity)
 
